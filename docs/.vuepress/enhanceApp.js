4a64d0419a3c5d256d2834f1c21a0cdfddec0878
avery0010 <951625730@qq.com>
2022-08-03 21:50:02 +0800

    fix: 5

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,11 +4,9 @@ export default ({ router }) => {
      */
     router.beforeEach((to, from, next) => {
         // console.log("切换路由", to.fullPath, from.fullPath);
-        
-        console.log(1212121212)
+
         //触发百度的pv统计
         if (typeof _hmt != "undefined") {
-            console.log(11111111);
             if (to.path) {
                 _hmt.push(["_trackPageview", to.fullPath]);
                 console.log("上报百度统计", to.fullPath);
