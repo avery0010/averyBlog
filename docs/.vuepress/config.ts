59c264afad6eb838b6b926139d8437aa922d12bd
avery0010 <951625730@qq.com>
2022-08-03 21:11:06 +0800

    fix: 1

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,109 +1,78 @@
 import { defineHopeConfig } from "vuepress-theme-hope";
 import themeConfig from "./themeConfig";
 
-// export default defineHopeConfig({
-//   base: "/", 
-//   title: "Avery",//默认信息
-//   description: "Avery的学习成长记录",//默认信息
-
-//   head: [
-//     [
-//       "link",
-//       {
-//         rel: "stylesheet",
-//         href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
-//       },
-//     ],
-//   //   //被搜索引擎搜索标签
-//   //   [
-//   //     "meta",
-//   //     {
-//   //       name: 'keywords',
-//   //       content: 'Avery,前端开发,Vue,React',
-//   //     }
-//   //   ],
-//   //   //添加百度站长的自动推送代码
-//   //   // [
-//   //   //   "script",
-//   //   //   {
-//   //   //     src: "/assets/js/autopush-baidu.js"
-//   //   //   }
-//   //   // ],  
-//   //   //360站长的验证代码
-//   //   [
-//   //     "meta",
-//   //     {
-//   //       name: "360-site-verification",
-//   //       content: "63b223c45248d2ea87cb9b1042148dda",
-//   //     },
-//   //   ],
-//   //   // 添加360站长的自动推送代码
-//   //   // [
-//   //   //   "script",
-//   //   //   {
-//   //   //     src: "/assets/js/autopush-360.js",
-//   //   //   },
-//   //   // ],
-//   //   ['script', {},
-//   //     `
-//   //     var _hmt = _hmt || [];
-//   //     (function() {
-//   //       var hm = document.createElement("script");
-//   //       hm.src = "https://hm.baidu.com/hm.js?469aa022c0181622bdf0357a8ca0fd9a";
-//   //       var s = document.getElementsByTagName("script")[0]; 
-//   //       s.parentNode.insertBefore(hm, s);
-//   //     })();
-//   //     `
-//   //  ],
-//   ],
-//   // <meta name="360-site-verification" content="63b223c45248d2ea87cb9b1042148dda" />
-
-  
-  
-
-//   // locales: {
-//   //   "/": {
-//   //     lang: "en-US",
-//   //     title: "Theme Demo",
-//   //     description: "A demo for vuepress-theme-hope",
-//   //   },
-//   //   "/zh/": {
-//   //     lang: "zh-CN",
-//   //     title: "主题演示",
-//   //     description: "vuepress-theme-hope 的演示",
-//   //   },
-//   // },
-
-//   themeConfig,
-// });
 export default defineHopeConfig({
-  port: 9927,
-  title: "Avery",
-  description: "Avery",
-  dest: "./dist",
+  base: "/",
+  title: "JavaPub",//默认信息
+  description: "专注于面试、副业，技术人的成长记录",//默认信息
+
   head: [
-    // 添加百度统计
-    ["script", {},
-      `var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?906138b9aada58204bf5d1a51d54abc9";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })();`
-    ],["link",{
+    [
+      "link",
+      {
         rel: "stylesheet",
         href: "//at.alicdn.com/t/font_2410206_mfj6e1vbwo.css",
       },
     ],
-    
+    //被搜索引擎搜索标签
+    [
+      "meta",
+      {
+        name: 'keywords',
+        content: 'JavaPub,java,爬虫,毕业设计,python,计算机毕业设计,elasticsearch,springboot',
+      }
+    ],
+    //添加百度站长的自动推送代码
+    [
+      "script",
+      {
+        src: "/assets/js/autopush-baidu.js"
+      }
+    ],  
+    //360站长的验证代码
+    [
+      "meta",
+      {
+        name: "360-site-verification",
+        content: "63b223c45248d2ea87cb9b1042148dda",
+      },
+    ],
+    // 添加360站长的自动推送代码
+    [
+      "script",
+      {
+        src: "/assets/js/autopush-360.js",
+      },
+    ],
+    ['script', {},
+      `
+      var _hmt = _hmt || [];
+      (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?469aa022c0181622bdf0357a8ca0fd9a";
+        var s = document.getElementsByTagName("script")[0]; 
+        s.parentNode.insertBefore(hm, s);
+      })();
+      `
+   ],
   ],
+  // <meta name="360-site-verification" content="63b223c45248d2ea87cb9b1042148dda" />
 
-  locales: {
-    "/": {
-      lang: "zh-CN"
-    },
-  },
+  
+  
+
+  // locales: {
+  //   "/": {
+  //     lang: "en-US",
+  //     title: "Theme Demo",
+  //     description: "A demo for vuepress-theme-hope",
+  //   },
+  //   "/zh/": {
+  //     lang: "zh-CN",
+  //     title: "主题演示",
+  //     description: "vuepress-theme-hope 的演示",
+  //   },
+  // },
 
   themeConfig,
 });
